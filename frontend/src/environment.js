ec4679a8c95821286bb71138bc89fc592e200540
Anjali <anjalii0914@gmail.com>
2025-09-10 01:45:49 +0530

    added backend deployment link

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,7 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://studymeetbackend.onrender.com/" : 
+     
+    "http://localhost:8000"
 
 export default server;
