b3247947312739ccc0e3021f387ffc1b0a6abd55
Anjali <anjalii0914@gmail.com>
2025-09-11 00:39:38 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import LandingPage from './pages/landing';
-import Authentication from './pages/authentication';
-import { AuthProvider } from './contexts/AuthContext';
-import VideoMeetComponent from './pages/VideoMeet';
+import LandingPage from './pages/landing.jsx';
+import Authentication from './pages/authentication.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import VideoMeetComponent from './pages/VideoMeet.jsx';
 import HomeComponent from './pages/home.jsx';
-import History from './pages/history';
+import History from './pages/history.jsx';
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
 
             <Route path='/auth' element={<Authentication />} />
 
-            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/home' element={<HomeComponent />} />
             <Route path='/history' element={<History />} />
             <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
